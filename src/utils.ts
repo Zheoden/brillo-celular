b61f81d4554c5c151925bbfb10ce7891c2085e44
Zheoden <schifferjulian@gmail.com>
2025-11-17 19:05:49 -0300

    remove comments

--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,4 @@
 export const ConversorLuxtoNits = (lux: number): number => {
-  // Example conversion formula (this is a placeholder; replace with actual formula)
   const result = 458.21 * Math.log(lux + 202.24) - 2355.58;
-  // limit to 2 decimals
   return Number(result.toFixed(2));
 };
